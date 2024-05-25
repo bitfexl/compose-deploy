17c07b8af2fe111ab5c6b42c92202384799764ea
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-05-25 18:45:04 +0200

    added tailwind

--- script-generator/src/App.tsx
+++ script-generator/src/App.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-    return <h1>Hello, World!</h1>;
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>Hello, World!</h2>
+            <h3>Hello, World!</h3>
+            <h4>Hello, World!</h4>
+            <p>Hello, World!</p>
+        </div>
+    );
 }
